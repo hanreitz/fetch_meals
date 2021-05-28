5bdc14e56f6010c7bb15121269da89b7d1991080
Aysan Isayo <aisayo@icloud.com>
2021-05-28 14:15:38 -0700

    add meal details

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/'
 const form = document.getElementById('search-form')
-let mealsList = document.getElementById("meals-list")
+const mealsList = document.getElementById("meals-list")
+const mealsDetails = document.querySelector("#meals-details")
 
 form.addEventListener('submit', searchMeals)
 
@@ -22,6 +23,36 @@ function renderMeals(mealsArr){
 }
 
 function renderMeal(meal){
-    const li = `<li id=${meal.idMeal}>${meal.strMeal}</li>`
-    mealsList.innerHTML += li
+    const a = document.createElement('a')
+    a.setAttribute('href', '#')
+    const li = document.createElement('li')
+    li.id = meal.idMeal
+    li.innerText = meal.strMeal
+    a.addEventListener('click', mealShow)
+    a.append(li)
+    mealsList.append(a)
+}
+
+function mealShow(event){
+    let mealId = event.target.id
+    fetchMeal(mealId)
+}
+
+function fetchMeal(mealId){
+    fetch(BASE_URL + `lookup.php?i=${mealId}`)
+    .then(resp => resp.json())
+    .then(meal => {
+        renderMealDetails(meal)
+    })
+}
+
+function renderMealDetails(meal){
+    let mealObj = meal.meals[0]
+    let header = document.createElement('h2')
+    let p = document.createElement('p')
+    header.innerText = mealObj.strMeal
+    p.innerText = `Instructions: ${mealObj.strInstructions}`
+    mealsDetails.innerHTML = ''
+    mealsDetails.append(header, p)
+
 }
